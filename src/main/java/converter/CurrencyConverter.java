47ff4ab4465b6eb507cf257effa9e943b036e568
Chitra2906 <chitradey29@gmail.com>
2022-01-24 22:11:13 +0530

    latest logger update 2.17.1

--- src/main/java/converter/CurrencyConverter.java
+++ src/main/java/converter/CurrencyConverter.java
@@ -1,33 +1,21 @@
-import java.io.FileInputStream;
+package converter;
 import java.util.HashMap;
-import java.util.Properties;
 import java.util.Scanner;
 
-import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 public class CurrencyConverter {
-	private static final String LOG_FILE = "log4j.properties";
 	public static void main(String[] args) {
-		
-		final Logger logger = Logger.getLogger(CurrencyConverter.class);
-		Properties properties = new Properties();
-		try {
-			properties.load(new FileInputStream(LOG_FILE));
-			PropertyConfigurator.configure(properties);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} 
-		
-		
-		
+
+		Logger logger = LogManager.getLogger(CurrencyConverter.class);
+
 		HashMap<Integer, String> CurrencyCode = new HashMap<Integer,String>();
 		CurrencyCode.put(1, "Euro");
 		CurrencyCode.put(2, "CAD");
 		CurrencyCode.put(3, "USD");
-		
+
 		HashMap<String,Float> CurrencyConversion = new HashMap<String, Float>();
 		CurrencyConversion.put("Euro", (float) 0.012);
 		CurrencyConversion.put("CAD",(float) 0.017);
@@ -48,7 +36,6 @@ public class CurrencyConverter {
 
 		Float rate = CurrencyConversion.get(toCode);
 		System.out.println(rate);
-
 		logger.info("User tried to convert INR to "+toCode+" for amount " + amount );
 		System.out.println("After conversion : " + amount*rate);
 		logger.info(toCode + " converted amount is " + amount*rate);
