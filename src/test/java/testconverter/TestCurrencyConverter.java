ddc0553084bdbb518abba447361403249793ad6d
Chitra2906 <chitradey29@gmail.com>
2022-01-24 23:40:16 +0530

    adding test folder

---
+++ src/test/java/testconverter/TestCurrencyConverter.java
@@ -0,0 +1,14 @@
+package testconverter;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCurrencyConverter {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
